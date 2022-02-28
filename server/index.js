6f7f0aac41a3fbf8b429cd080e0c43566eebebad
Pranav Singh <pranavsingh02@hotmail.com>
2022-02-28 20:38:15 +0530

    add boiler plate code for backend

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = 4000;
+
+const app = express();
+app.use(express.urlencoded({
+    extends: true
+}));
+app.use(cors());
+app.use(express.json());
+
+app.use("/", async (req, res) => {
+    res.write('<h1>Backend server running!</h1>')
+    res.send();
+})
+
+app.listen(PORT, () => console.log(`App is listening at ${PORT}`));
+
